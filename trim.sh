cf02a932e133bb40cb5c29f465ea11fa9157fe30
Anderson Lira <anderson.lira@celfocus.com>
2019-04-11 09:44:43 +0100

    Easy checkout git added. Trim function added

---
+++ trim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+trim() {
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"   
+    echo -n "$var"
+}
