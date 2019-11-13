2c447512f37216ed584946f31746ec121f3d453e
Anderson Lira <anderson.lira@celfocus.com>
2019-11-13 12:45:48 +0000

    #AL # master | isntall jar maven repository added

---
+++ maven/install_jar_local_repository.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# install jar as artifact repository
+ mvn install:install-file -Dfile=<path-to-file> -DgroupId=<group-id> -DartifactId=<artifact-id> -Dversion=<version> -Dpackaging=<packaging>
