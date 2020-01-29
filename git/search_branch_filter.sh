841945437a4c76175a123b2a6d9a001be427465b
Anderson Lira <anderson.lira@celfocus.com>
2020-01-29 11:17:38 +0000

    other filters added

--- git/search_branch_filter.sh
+++ git/search_branch_filter.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-#filter by date and author
+#filter by date and
 git log --pretty=format:"%ad - %an: %s" --after="2016-01-31" --until="2017-03-10" 
-
+#author
+git log --author="John\|Mary"
+#message
+git log --grep="JRA-224:"
+#file
+git log -- foo.py bar.py
+#content
+git log -S"Hello, World!"
