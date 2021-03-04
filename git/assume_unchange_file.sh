83cf7b5182266c4ba3e318f730811bee2cea9cb5
Anderson Lira <anderson.lira@celfocus.com>
2021-03-04 14:53:19 +0000

    assume unchanges git ok

---
+++ git/assume_unchange_file.sh
@@ -0,0 +1,3 @@
+git update-index --assume-unchanged <file> 
+
+git update-index --no-assume-unchanged <file> packages/digital.cms.apps/src/main/content/jcr_root/apps/digital-commons/clientlibs/vendor/vue/js.txt
