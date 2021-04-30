a9b4cff35db9ba698317501990fa8d2091634303
Anderson Lira <anderson.lira@celfocus.com>
2021-04-30 16:02:56 +0100

    map example added

--- loop_array.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-arr=( "A" "B" "C" )
-
-for var in "${arr[@]}"
-do
-  echo "${var}"
-  # do something on $var
-done
---
+++ loop_map_array.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+arr=( "A" "B" "C" )
+
+for var in "${arr[@]}"
+do
+  echo "${var}"
+  # do something on $var
+done
+
+
+#map
+declare -A animals=( ["moo"]="cow" ["woof"]="dog" ["meow"]="cat" )
+
+echo "${animals[moo]}"
+
+for sound in "${!animals[@]}"
+do 
+  echo "$sound - ${animals[$sound]}" 
+done
+
